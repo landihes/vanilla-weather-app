8790cb79d2dec52b23207b7b6d406fae555d8072
landihes <107809774+landihes@users.noreply.github.com>
2022-06-26 13:29:10 +0100

    JS current time feature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,49 @@
+// challange 1
 
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let currentDay = days[date.getDay()];
+  let currentMonth = months[date.getMonth()];
+  let currentDate = date.getDate();
+  let currentHours = date.getHours();
+  if (currentHours < 10) {
+    currentHours = `0${currentHours}`;
+  }
+  let currentMinutes = date.getMinutes();
+  if (currentMinutes < 10) {
+    currentMinutes = `0${currentMinutes}`;
+  }
+
+  let formattedDate = `${currentDay} ${currentDate} ${currentMonth}, ${currentHours}:${currentMinutes}`;
+  return formattedDate;
+}
+
+//
+
+let currentTime = new Date();
+let cityDate = document.querySelector("#date-input");
+cityDate.innerHTML = formatDate(currentTime);
