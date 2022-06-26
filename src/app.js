4a58f8832ff3e3d3984848f3cc2b04db3dcc0c47
landihes <107809774+landihes@users.noreply.github.com>
2022-06-26 14:04:50 +0100

    JS Location Update

--- src/app.js
+++ src/app.js
@@ -47,3 +47,39 @@ function formatDate(date) {
 let currentTime = new Date();
 let cityDate = document.querySelector("#date-input");
 cityDate.innerHTML = formatDate(currentTime);
+
+// change location + weather
+
+function displayWeatherCondition(response) {
+  console.log(response.data.name);
+  document.querySelector("h1").innerHTML = response.data.name;
+  document.querySelector("#temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function searchCity(city) {
+  let apiKey = "75bbb29ab49fdad9555644e1ff00d35d";
+  let apiUlr = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUlr).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+searchCity("London");
+
+//
