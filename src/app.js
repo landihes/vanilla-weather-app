b6a54eb123d6a100a92a393189d36038de5afdb1
landihes <107809774+landihes@users.noreply.github.com>
2022-06-27 17:43:10 +0100

    Forecast / API Integration in JS
    Needs to be formatted in CSS - blue to highlight the data

--- src/app.js
+++ src/app.js
@@ -23,30 +23,63 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
   let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
       <div class="col">
         <div class="WeatherForecastPreview">
-            <div class="forecast-date"> ${day} </div>
-                <img src=""http://openweathermap.org/img/wn/50d@2x.png" alt="" id="icon" class="float-left" />
+            <div class="forecast-date"> ${formatDay(forecastDay.dt)} </div>
+   
+                <img src="http://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png" alt="" width="42" />
                   <div class="forecast-temperature">
-                    <span class="forecast-temperature-max">30°</span
-                    ><span class="forecast-temperature-min">22°</span>
-                  </div>
-                </div>
-              </div>
-        `;
+                    <span class="forecast-temperature-max">
+                    ${Math.round(forecastDay.temp.max)}</span>
+                    <span class="forecast-temperature-min"> ${Math.round(
+                      forecastDay.temp.min
+                    )}</span>
+             </div>
+            </div>
+        </div>
+       `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "75bbb29ab49fdad9555644e1ff00d35d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -71,6 +104,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -121,4 +156,3 @@ fahrenheitLink.addEventListener("click", displayFharenheitTemperature);
 //
 
 search("London");
-displayForecast();
